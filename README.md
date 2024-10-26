a5a8e86f76d1b1e2d50afb63a212abbddb41828a
leodoeio <basicleo787@gmail.com>
2024-10-27 01:56:55 +0530

    Updated frontend now working as intended

---
+++ README.md
@@ -0,0 +1,20 @@
+## to run application
+
+## backend chat bot
+
+```
+cd bot_chatpilot
+poetry shell
+poetry install
+uvicorn main:app --reload
+```
+
+## cd ui_chatpilot
+
+```
+npm i -g pnpm
+pnpm install
+pnpm run dev
+```
+
+you can acess the website at http://127.0.0.1:5173/
--- ui_chatpilot/src/App.jsx
+++ ui_chatpilot/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { CiLink } from "react-icons/ci";
+
 function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+    <div className="flex h-screen items-center justify-center bg-black gap-1">
+      <p className="text-sm text-white">
+        Say this is the website say wikipidia of
+      </p>
+      <p className="font-bold text-white inline">ReactJs</p>
+      <a href="https://en.wikipedia.org/wiki/React_(JavaScript_library)">
+        <CiLink  className="text-blue-500 inline ml-1" size={25} />
+      </a>
+    </div>
   );
 }
 
