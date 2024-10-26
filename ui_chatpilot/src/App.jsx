be9997f556e44ad8c7c0413eb6b7cf7c55048fa0
leodoeio <basicleo787@gmail.com>
2024-10-27 02:09:52 +0530

    updaeted sampel header

--- ui_chatpilot/src/App.jsx
+++ ui_chatpilot/src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import { CiLink } from "react-icons/ci";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-black gap-1">
-      <p className="text-sm text-white">
-        Say this is the website say wikipidia of
-      </p>
-      <p className="font-bold text-white inline">ReactJs</p>
-      <a href="https://en.wikipedia.org/wiki/React_(JavaScript_library)">
-        <CiLink  className="text-blue-500 inline ml-1" size={25} />
-      </a>
+    <div className="h-screen w-screen bg-black flex flex-col items-center justify-center">
+      <Header />
+        <p className="text-sm text-white">
+          Say this is the wikipedia page of
+        </p>
+        <p className="font-bold text-white inline">ReactJs</p>
+        <a href="https://en.wikipedia.org/wiki/React_(JavaScript_library)">
+          <CiLink className="text-blue-500 inline ml-1" size={25} />
+        </a>
     </div>
   );
 }
