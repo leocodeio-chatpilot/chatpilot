7e4677412446229ee0bb78eb27dca1effb13b880
Harshax <basicleo787@gmail.com>
2024-11-16 19:52:45 +0530

    initialized ui for marketing

---
+++ webapp_chatpilot/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ToggleButton } from "../context/ThemeToggle";
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold dark:text-white">hello</p>
+      <ToggleButton />
+    </>
+  );
+}
+
+export default App;
