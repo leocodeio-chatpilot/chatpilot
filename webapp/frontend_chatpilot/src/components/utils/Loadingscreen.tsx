3e1708b4ed0f2cf282de5d6ce10952d31af3357a
lochankumarsunkara <lsunkara@netlabsaustralia.com>
2024-12-11 11:39:21 +0530

    added loading screen

--- webapp/frontend_chatpilot/src/components/utils/Loadingscreen.tsx
+++ webapp/frontend_chatpilot/src/components/utils/Loadingscreen.tsx
@@ -3,7 +3,7 @@
 const LoadingScreen = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-secondary"></div>
+      <div className="animate-spin rounded-full h-12 border-t-2 border-b-2 border-secondary"></div>
     </div>
   );
 };
